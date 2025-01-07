97021e05fd0b0dc14e7cb765a46dc901eed96df6
carlodizon03 <carlodizon03@gmail.com>
2025-01-07 21:15:11 +0800

    ignore

--- src/index.ts
+++ src/index.ts
@@ -6,25 +6,13 @@ import { Pool, PoolConfig, QueryResult } from 'pg';
  */
 export class HBDRepo {
   private static instance: HBDRepo | null = null;
-<<<<<<< HEAD
-  private pool: ConnectionPool | null = null;
-  private sqlConfig: SqlConfig;
-=======
   private pool: Pool | null = null;
   private poolConfig: PoolConfig;
->>>>>>> 66fb39699e3a0d1d1bae8c19aebaaa9772d74b0e
 
   /**
    * Private constructor to ensure only one instance of the repository is created.
    * @param poolConfig - The PostgreSQL pool configuration object.
    */
-<<<<<<< HEAD
-  private constructor(sqlConfig: SqlConfig) {
-    if (!sqlConfig) {
-      throw new Error('SQL configuration is required to initialize Database.');
-    }
-    this.sqlConfig = sqlConfig;
-=======
   private constructor(poolConfig: PoolConfig) {
     if (!poolConfig) {
       throw new Error(
@@ -32,25 +20,15 @@ export class HBDRepo {
       );
     }
     this.poolConfig = poolConfig;
->>>>>>> 66fb39699e3a0d1d1bae8c19aebaaa9772d74b0e
   }
   /**
    * Get the singleton instance of the repository.
-<<<<<<< HEAD
-   * @param sqlConfig - The SQL configuration object (only used for initialization).
-   * @returns The singleton instance of the repository.
-   */
-  public static getInstance(sqlConfig: SqlConfig): HBDRepo {
-    if (!HBDRepo.instance) {
-      HBDRepo.instance = new HBDRepo(sqlConfig);
-=======
    * @param poolConfig - The PostgreSQL pool configuration object (only used for initialization).
    * @returns The singleton instance of the repository.
    */
   public static getInstance(poolConfig: PoolConfig): HBDRepo {
     if (!HBDRepo.instance) {
       HBDRepo.instance = new HBDRepo(poolConfig);
->>>>>>> 66fb39699e3a0d1d1bae8c19aebaaa9772d74b0e
     }
     return HBDRepo.instance;
   }
@@ -69,58 +47,19 @@ export class HBDRepo {
       await this.connect();
     }
     try {
-<<<<<<< HEAD
-      const request = this.pool!.request(); // Use non-null assertion since `this.pool` is ensured to exist.
-      if (params) {
-        params.forEach((param, index) => {
-          request.input(`param${index + 1}`, param);
-        });
-      }
-      const result = await request.query<T>(queryString);
-      return result.recordset;
-=======
       const result: QueryResult<T> = await this.pool!.query(
         queryString,
         params
       );
       return result.rows;
->>>>>>> 66fb39699e3a0d1d1bae8c19aebaaa9772d74b0e
     } catch (error) {
       console.error('Query execution failed:', error);
       throw new Error('Failed to execute query.');
     }
   }
-  /**
-   * Connect to the database.
-   * @returns A Promise resolving to the connection pool.
-   */
-  private async connect() {
-    if (!this.pool) {
-      try {
-        const connectionPool = new sql.ConnectionPool(this.sqlConfig);
-        this.pool = await connectionPool.connect();
-        console.log('Database connected');
-      } catch (err) {
-        console.error('Database connection error:', err);
-        throw err;
-      }
-    }
-    return this.pool;
-  }
+ 
 
   /**
-<<<<<<< HEAD
-   * Get the database connection pool.
-   * @returns The connection pool.
-   */
-  public async getConnection(): Promise<ConnectionPool> {
-    if (!this.pool) {
-      await this.connect();
-    }
-    return this.pool!;
-  }
-  
-=======
    * Connect to the database.
    * @returns A Promise resolving to the connection pool.
    */
@@ -137,7 +76,6 @@ export class HBDRepo {
     return this.pool;
   }
 
->>>>>>> 66fb39699e3a0d1d1bae8c19aebaaa9772d74b0e
   /**
    * Retrieves all HBD savings deposit transactions for a specific user.
    * @param username - The username of the account.
@@ -145,40 +83,19 @@ export class HBDRepo {
    */
   async deposits(username: string): Promise<any[]> {
     const queryString = `
-<<<<<<< HEAD
-      SELECT tx_id, [type], [from], [to], [amount], [timestamp]
-      FROM TxTransfers
-      WHERE ([from] = @param1 OR [to] = @param1) 
-        AND [type] = 'transfer_to_savings'
-        AND [amount_symbol] = 'HBD'
-      ORDER BY [timestamp] ASC;
-=======
           SELECT *, hafsql.get_timestamp(id) AS timestamp
         FROM hafsql.operation_transfer_to_savings_table
         WHERE (from_account = $1 or  to_account = $1 ) AND symbol = 'HBD' 
          ORDER BY timestamp ASC;
->>>>>>> 66fb39699e3a0d1d1bae8c19aebaaa9772d74b0e
     `;
     return await this.query(queryString, [username]);
   }
 
-<<<<<<< HEAD
-   /**
-=======
   /**
->>>>>>> 66fb39699e3a0d1d1bae8c19aebaaa9772d74b0e
    * Retrieves all HBD savings withdrawal transactions for a specific user.
    * @param username - The username of the account.
    * @returns A Promise resolving to an array of withdrawal transactions.
    */
-<<<<<<< HEAD
-  async withrawals(username: string): Promise<any[]> {
-    const queryString = `
-      SELECT * FROM VOFillTransferFromSavings 
-      where ([from]=@param1 or [to]=@param1)
-        AND [amount_symbol] = 'HBD'
-	    ORDER BY [timestamp] ASC;
-=======
   async withdrawals(username: string): Promise<any[]> {
     const queryString = `
          SELECT * , hafsql.get_timestamp(id) AS timestamp
@@ -186,7 +103,6 @@ export class HBDRepo {
           WHERE from_account = $1
           AND symbol = 'HBD' ORDER BY timestamp ASC;
 
->>>>>>> 66fb39699e3a0d1d1bae8c19aebaaa9772d74b0e
     `;
     return await this.query(queryString, [username]);
   }
@@ -198,17 +114,9 @@ export class HBDRepo {
    */
   async totalDeposit(username: string): Promise<number> {
     const queryString = `
-<<<<<<< HEAD
-      SELECT SUM([amount]) AS total_amount
-      FROM TxTransfers
-      WHERE ([from] = @param1 OR [to] = @param1)
-        AND [type] = 'transfer_to_savings'
-        AND [amount_symbol] = 'HBD';
-=======
       SELECT SUM(amount) as total_amount
       FROM hafsql.operation_transfer_to_savings_table
       WHERE (from_account = $1 or  to_account = $1 ) AND symbol = 'HBD' 
->>>>>>> 66fb39699e3a0d1d1bae8c19aebaaa9772d74b0e
     `;
     const result = await this.query<{ total_amount: number }>(queryString, [
       username,
@@ -216,28 +124,17 @@ export class HBDRepo {
     return result[0]?.total_amount || 0;
   }
 
-<<<<<<< HEAD
-    /**
-=======
   /**
->>>>>>> 66fb39699e3a0d1d1bae8c19aebaaa9772d74b0e
    * Calculates the total amount withdrawn from HBD savings for a specific user.
    * @param username - The username of the account.
    * @returns A Promise resolving to the total withdrawn amount.
    */
   async totalWithdrawal(username: string): Promise<number> {
     const queryString = `
-<<<<<<< HEAD
-      SELECT SUM([amount]) AS total_amount
-      FROM VOFillTransferFromSavings 
-      where ([from] = @param1 or  [to]=@param1)
-        AND [amount_symbol] = 'HBD';
-=======
      SELECT SUM(amount) AS total_amount
       FROM hafsql.operation_fill_transfer_from_savings_table
       WHERE from_account = $1
         AND symbol= 'HBD'
->>>>>>> 66fb39699e3a0d1d1bae8c19aebaaa9772d74b0e
     `;
     const result = await this.query<{ total_amount: number }>(queryString, [
       username,
